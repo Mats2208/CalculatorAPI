7e2986bbe85bbf97d9f93e1dc0b3a4a03b9f29a6
Mateo <mateoandres2208@gmail.com>
2025-08-20 19:58:21 -0400

    feat: remove additional files and creating basis endpoint

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("sum")]
+        public IActionResult GetSum([FromQuery] int a, [FromQuery] int b)
+        {
+            var result = a + b;
+            return Ok(new { Result = result });
+        }
+
+    }
+}
