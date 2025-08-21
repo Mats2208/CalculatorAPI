2ef7fbcd2a44bebdaa6cd7fdf223a309a0fdb92a
Mateo <mateoandres2208@gmail.com>
2025-08-20 20:03:11 -0400

    feat(calculator): add endpoints for subtraction, multiplication and division

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -8,11 +8,38 @@ namespace CalculatorAPI.Controllers
     public class CalculatorController : ControllerBase
     {
         [HttpGet("sum")]
-        public IActionResult GetSum([FromQuery] int a, [FromQuery] int b)
+        public IActionResult GetSum([FromQuery] double a, [FromQuery] double b)
         {
             var result = a + b;
             return Ok(new { Result = result });
         }
 
+        [HttpGet("decrease")]
+        public IActionResult GetDecrease([FromQuery] double a, [FromQuery] double b)
+        {
+            var result = a - b;
+            return Ok(new { Result = result });
+        }
+
+        [HttpGet("divide")]
+        public IActionResult GetDivide([FromQuery] double a, [FromQuery] double b)
+        {
+            if (b == 0)
+            {
+                return BadRequest("Division by zero is not allowed.");
+            }
+            else
+            {
+                double result = a / b;
+                return Ok(new { Result = result });
+            }
+        }
+
+        [HttpGet("mult")]
+        public IActionResult GetMult([FromQuery] double a, [FromQuery] double b)
+        {
+            var result = a * b;
+            return Ok(new { Result = result });
+        }
     }
 }
